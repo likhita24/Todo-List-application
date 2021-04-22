fe2ec03a27755a29186f9d8144f598564e8e797d
likhita24 <srilikitha.bitra@gmail.com>
2021-04-22 14:30:03 +0530

    pytest installation and checks

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'todo_app.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'todoapp',
-        'USER': 'likhita07',
-        'PASSWORD': '2001$likitha',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- todo_app/todopages/models.py
+++ todo_app/todopages/models.py
@@ -5,6 +5,9 @@ class users(models.Model):
     username = models.CharField(max_length = 100)
     password = models.CharField(max_length = 100)
 
+    def __str__(self):
+        return self.username
+
 class todoitems(models.Model):
     Task_name = models.CharField(max_length = 100)
     user_id = models.ForeignKey(users, on_delete = models.CASCADE)
